f86d6ccd343f428acc403860613dca309015f0c9
Sam <sam.gordon@valpo.edu>
2021-11-04 17:10:00 -0500

    Solution

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] array1 = {0,1,0,3,12};
+        int[] array2 = {0};
+        int[] array3 = {0,0,1};
+        int[] array4 = {1};
+        int[] array5 = {0,1};
+
+        solution.moveZeros(array1);
+        solution.moveZeros(array2);
+        solution.moveZeros(array3);
+        solution.moveZeros(array4);
+        solution.moveZeros(array5);
+    }
+}
